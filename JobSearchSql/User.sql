bfc5ff8306153d07d879961be161a524b074bee2
natchiarot <142550205+natchiarot@users.noreply.github.com>
2024-04-04 17:36:37 -0230

    Add files via upload

---
+++ JobSearchSql/User.sql
@@ -0,0 +1,15 @@
+insert into User (UserName, Position, Email, Phone, Location) values ('Sarah Davis', 'Engineering Manager', 'john.doe@example.com', '607-140-2479', '202 Maple Ln');
+insert into User (UserName, Position, Email, Phone, Location) values ('David Thomas', 'Business Development Manager', 'user456@example.com', '896-847-6459', '456 Elm St');
+insert into User (UserName, Position, Email, Phone, Location) values ('Emily Johnson', 'Chief Technology Officer (CTO)', 'example.email@example.com', '696-501-4546', '101 Pine Rd');
+insert into User (UserName, Position, Email, Phone, Location) values ('Daniel Martinez', 'Chief Technology Officer (CTO)', 'example.email@example.com', '044-657-4935', '456 Elm St');
+insert into User (UserName, Position, Email, Phone, Location) values ('William Anderson', 'IT Manager', 'example.email@example.com', '470-598-2220', '789 Oak Ave');
+insert into User (UserName, Position, Email, Phone, Location) values ('Robert Wilson', 'Chief Information Officer (CIO)', 'user456@example.com', '182-760-8565', '202 Maple Ln');
+insert into User (UserName, Position, Email, Phone, Location) values ('Emily Johnson', 'Director of User Experience', 'example.email@example.com', '048-589-2129', '101 Pine Rd');
+insert into User (UserName, Position, Email, Phone, Location) values ('Jessica Taylor', 'IT Manager', 'user456@example.com', '617-412-2987', '202 Maple Ln');
+insert into User (UserName, Position, Email, Phone, Location) values ('James Hernandez', 'Technical Lead', 'testuser123@example.com', '087-366-6211', '202 Maple Ln');
+insert into User (UserName, Position, Email, Phone, Location) values ('David Thomas', 'Software Development Manager', 'user456@example.com', '913-868-8682', '456 Elm St');
+insert into User (UserName, Position, Email, Phone, Location) values ('Laura Rodriguez', 'Product Manager', 'user456@example.com', '419-874-7561', '123 Main St');
+insert into User (UserName, Position, Email, Phone, Location) values ('Amanda Garcia', 'Director of Operations', 'example.email@example.com', '721-827-4042', '456 Elm St');
+insert into User (UserName, Position, Email, Phone, Location) values ('Laura Rodriguez', 'Chief Technology Officer (CTO)', 'user456@example.com', '334-018-3590', '456 Elm St');
+insert into User (UserName, Position, Email, Phone, Location) values ('Jessica Taylor', 'Director of Information Technology', 'john.doe@example.com', '521-190-1113', '456 Elm St');
+insert into User (UserName, Position, Email, Phone, Location) values ('John Smith', 'Director of Information Technology', 'user456@example.com', '376-507-7348', '101 Pine Rd');
