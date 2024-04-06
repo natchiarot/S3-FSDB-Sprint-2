fd5e810b0adc5f7ec01142963f2d8d302adcfe49
laurenwilson27 <140430039+laurenwilson27@users.noreply.github.com>
2024-04-06 03:52:21 -0230

    full text search (using unindexed vector)

--- routes/resumes.js
+++ routes/resumes.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("resumesIndex");
+const pgDal = require("../services/resumes.pg.dal");
+
+// GET to "resumes/" renders a list of all resumes
+// In the future, this should instead display a form to get search terms from the user
+router.get("/", async (req, res) => {
+  // The DAL can potentially throw errors, handle them here with try/catch.
+  try {
+    const resumes = await pgDal.getAllResumes();
+
+    res.render("resumesIndex", { resumes: resumes });
+  } catch (e) {
+    res
+      .status(503)
+      .end("There was an error when attempting to access the database: " + e);
+  }
 });
 
-router.get("/search", (req, res) => {
-  res.render("resumesSearch");
+// GET to "resumes/search" expects a 'query' parameter containing words to search for.
+// For instance, resumes/search?query=node%20react
+router.get("/search", async (req, res) => {
+  // If there is no query, behaviour is different
+  if (!req.query.query) {
+    res.status(400).end("Query must include search query.");
+  } else {
+    try {
+      // Search query is separated by whitespace, split the query into an array
+      const terms = req.query.query.split(/\s+/);
+
+      const resumes = await pgDal.searchAllResumes(terms);
+
+      res.render("resumesSearch", { query: req.query.query, resumes: resumes });
+    } catch (e) {
+      res
+        .status(503)
+        .end("There was an error when attempting to access the database: " + e);
+    }
+  }
 });
 
 module.exports = router;
