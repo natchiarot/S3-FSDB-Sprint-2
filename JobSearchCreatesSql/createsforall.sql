b5fc9fc2e13aad4577356c5119553d16a9e43f3a
laurenwilson27 <140430039+laurenwilson27@users.noreply.github.com>
2024-04-12 12:52:42 -0230

    Added UNIQUE constraint for username, deleted mock data which violates constraint

--- JobSearchCreatesSql/createsforall.sql
+++ JobSearchCreatesSql/createsforall.sql
@@ -1,30 +1,25 @@
 -- Create User table
 CREATE TABLE "User" (
     User_ID SERIAL PRIMARY KEY,
-    UserName VARCHAR(255),
+    UserName VARCHAR(255) NOT NULL UNIQUE,
     Position VARCHAR(255),
     Email VARCHAR(255),
     Phone VARCHAR(15),
     Location VARCHAR(255),
     Password VARCHAR(255),
-    UNIQUE (UserName)
 );
 
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('Sarah Davis', 'Engineering Manager', 'john.doe@example.com', '607-140-2479', '202 Maple Ln');
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('David Thomas', 'Business Development Manager', 'user456@example.com', '896-847-6459', '456 Elm St');
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('Emily Johnson', 'Chief Technology Officer (CTO)', 'example.email@example.com', '696-501-4546', '101 Pine Rd');
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('Daniel Martinez', 'Chief Technology Officer (CTO)', 'example.email@example.com', '044-657-4935', '456 Elm St');
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('William Anderson', 'IT Manager', 'example.email@example.com', '470-598-2220', '789 Oak Ave');
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('Robert Wilson', 'Chief Information Officer (CIO)', 'user456@example.com', '182-760-8565', '202 Maple Ln');
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('Emily Johnson', 'Director of User Experience', 'example.email@example.com', '048-589-2129', '101 Pine Rd');
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('Jessica Taylor', 'IT Manager', 'user456@example.com', '617-412-2987', '202 Maple Ln');
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('James Hernandez', 'Technical Lead', 'testuser123@example.com', '087-366-6211', '202 Maple Ln');
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('David Thomas', 'Software Development Manager', 'user456@example.com', '913-868-8682', '456 Elm St');
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('Laura Rodriguez', 'Product Manager', 'user456@example.com', '419-874-7561', '123 Main St');
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('Amanda Garcia', 'Director of Operations', 'example.email@example.com', '721-827-4042', '456 Elm St');
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('Laura Rodriguez', 'Chief Technology Officer (CTO)', 'user456@example.com', '334-018-3590', '456 Elm St');
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('Jessica Taylor', 'Director of Information Technology', 'john.doe@example.com', '521-190-1113', '456 Elm St');
-INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('John Smith', 'Director of Information Technology', 'user456@example.com', '376-507-7348', '101 Pine Rd');
+INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('SarahDavis', 'Engineering Manager', 'john.doe@example.com', '607-140-2479', '202 Maple Ln');
+INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('DavidThomas', 'Business Development Manager', 'user456@example.com', '896-847-6459', '456 Elm St');
+INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('EmilyJohnson', 'Chief Technology Officer (CTO)', 'example.email@example.com', '696-501-4546', '101 Pine Rd');
+INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('DanielMartinez', 'Chief Technology Officer (CTO)', 'example.email@example.com', '044-657-4935', '456 Elm St');
+INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('WilliamAnderson', 'IT Manager', 'example.email@example.com', '470-598-2220', '789 Oak Ave');
+INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('RobertWilson', 'Chief Information Officer (CIO)', 'user456@example.com', '182-760-8565', '202 Maple Ln');
+INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('JessicaTaylor', 'IT Manager', 'user456@example.com', '617-412-2987', '202 Maple Ln');
+INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('JamesHernandez', 'Technical Lead', 'testuser123@example.com', '087-366-6211', '202 Maple Ln');
+INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('LauraRodriguez', 'Product Manager', 'user456@example.com', '419-874-7561', '123 Main St');
+INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('AmandaGarcia', 'Director of Operations', 'example.email@example.com', '721-827-4042', '456 Elm St');
+INSERT INTO "User" (UserName, Position, Email, Phone, Location) VALUES ('JohnSmith', 'Director of Information Technology', 'user456@example.com', '376-507-7348', '101 Pine Rd');
 
 
 -- Create Search_Logs table
